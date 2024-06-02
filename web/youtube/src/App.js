7ca28758a00a9b21c8e257fea5483ed7e28634e3
guybaruch1 <113520009+guybaruch1@users.noreply.github.com>
2024-06-02 19:10:29 +0300

    init

--- web/youtube/src/App.js
+++ web/youtube/src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import AuthBox from './AuthBox';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        <button type="button" class="btn btn-primary">Primary</button>
-
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
       </header>
+      <body>
+        <AuthBox />
+      </body>
     </div>
   );
 }
