0d35cb89c1a3447296b80a996f6082d922532d0a
guybaruch1 <113520009+guybaruch1@users.noreply.github.com>
2024-06-02 11:39:26 +0300

    init

--- web/youtube/src/App.js
+++ web/youtube/src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+        <button type="button" class="btn btn-primary">Primary</button>
+
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
