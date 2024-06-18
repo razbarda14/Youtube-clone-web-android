6437d9e1c8bbffc2c947391622ff2b5349958115
razbarda14 <130838695+razbarda14@users.noreply.github.com>
2024-06-18 18:27:12 +0300

    before merge3

--- web/youtube/src/index.js
+++ web/youtube/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './ScreenMode/ThemeContext';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,4 +14,4 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-reportWebVitals();
+
