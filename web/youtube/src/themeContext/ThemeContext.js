153e9a3b529527b78c5c50674d49d04f2ac9d80c
TomBar2 <tomer.barak2@gmail.com>
2024-06-24 15:11:44 +0300

    Dark mode using useState + verything else works fine Needed: don't make username case sensitive and check if a username already exists

--- web/youtube/src/themeContext/ThemeContext.js
+++ web/youtube/src/themeContext/ThemeContext.js
@@ -1,5 +1,4 @@
-// src/ScreenMode/ThemeContext.js
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 
 const ThemeContext = createContext();
 
@@ -8,19 +7,8 @@ export const useTheme = () => useContext(ThemeContext);
 export const ThemeProvider = ({ children }) => {
     const [darkMode, setDarkMode] = useState(false);
 
-    useEffect(() => {
-        const savedTheme = localStorage.getItem('darkMode');
-        if (savedTheme) {
-            setDarkMode(JSON.parse(savedTheme));
-        }
-    }, []);
-
     const toggleTheme = () => {
-        setDarkMode(prevMode => {
-            const newMode = !prevMode;
-            localStorage.setItem('darkMode', JSON.stringify(newMode));
-            return newMode;
-        });
+        setDarkMode(prevMode => !prevMode);
     };
 
     return (
