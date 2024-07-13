82c2b7e398373057110cbb646e977182d8c396af
TomBar2 <tomer.barak2@gmail.com>
2024-07-14 00:11:47 +0300

    Putting users, videos and comments on our application

---
+++ web/server/routes/router.js
@@ -0,0 +1,37 @@
+const userController = require('../controllers/userController');
+const videoController = require('../controllers/videoController.js');
+const upload = require('../middleware/multerConfig');
+const authenticateToken = require('../middleware/authMiddleware'); // Correct import
+const express = require('express');
+
+const router = express.Router();
+
+router.route('/')
+    .post(userController.createUser);
+
+router.route('/getUserId')
+    .get(userController.getUserIdByUsername);
+
+router.route('/:id')
+    .get(authenticateToken, userController.getUserById);
+
+router.route('/:id')
+    .put(authenticateToken, userController.updateUser);
+
+router.route('/:id')
+    .delete(authenticateToken, userController.deleteUser);
+
+router.route('/:id/getDisplayName')
+    .get(userController.getUserDisplayName);
+
+router.route('/:id/getImagePath')
+    .get(userController.getUserImagePath);
+
+router.route('/:id/videos')
+    .get(videoController.getVideosByUploader);
+
+router.post('/register', upload.single('photo'), userController.registerUser);
+// router.post('/login', userController.loginUser);
+router.get('/verify-user', authenticateToken, userController.getUserById);
+
+module.exports = router;
